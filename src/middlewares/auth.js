55f7c128fe5245cb06fe07a91f498e05d7f7409e
NguyenPhuocDaiToan <daitoan2000@gmail.com>
2025-11-22 13:51:35 +0700

    update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -11,13 +11,13 @@ const verifyCallback = (req, resolve, reject, requiredRights) => async (err, use
   }
   req.user = user;
   // Logic check quyền (nếu cần uncomment sau này)
-  if (requiredRights.length) {
-    const userRights = roleRights.get(user.role);
-    const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
-    if (!hasRequiredRights && req.params.userId !== user.id) {
-      return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
-    }
-  }
+  // if (requiredRights.length) {
+  //   const userRights = roleRights.get(user.role);
+  //   const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
+  //   if (!hasRequiredRights && req.params.userId !== user.id) {
+  //     return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
+  //   }
+  // }
   resolve();
 };
 
